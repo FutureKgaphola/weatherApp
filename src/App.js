c80b9df9bdfa8915056dbb3802c64ae08bf8f6cb
future <futurekgaphola@gmail.com>
2023-07-20 10:39:51 +0200

    beta version of weather app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import { useState } from 'react';
 import dark from './assets/dark.jpg'
 import axios from 'axios';
 import { message } from 'antd';
+import brokenClouds from './assets/cloudy.png';
+import rain from './assets/rain.png';
+import thunder from './assets/thunder.png';
+import sunny from './assets/sunny.png';
+import foggy from './assets/foggy.png';
+import clear from './assets/clear.png';
 
 function App() {
   const [data,setData]=useState({});
@@ -11,7 +17,7 @@ function App() {
   const getdata= async()=>{
     if(place!==''){
       try {
-        var key= '4c1db660706a03b19ffd146e2f9a9f9c';
+        var key= process.env.REACT_APP_API_KEY;
         var url=`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=${key}`;
         var responce= await axios.get(url);
         setData(responce.data);
@@ -64,6 +70,32 @@ function App() {
            onClick={()=>getdata()} style={{margin:'5px'}} type="button" className="btn btn-success btn-sm">Search weather conditions</button>
           </div>
           {contextHolder}
+          {
+            data.main && data.weather[0].description!=='' && (data.weather[0].description).toLowerCase().includes('sun') ? 
+            <img style={{margin:'3px'}} src={sunny} alt="..." width="40px" height="40px"/> : null
+          }
+          {
+            data.main && data.weather[0].description!=='' && ((data.weather[0].description).toLowerCase().includes('mist') || (data.weather[0].description).toLowerCase().includes('fog')) ? 
+            <img style={{margin:'3px'}} src={foggy} alt="..." width="40px" height="40px"/> : null
+          }
+          
+          {
+            data.main && data.weather[0].description!=='' && (data.weather[0].description).toLowerCase().includes('clear sky') ? 
+            <img style={{margin:'3px'}} src={clear} alt="..." width="40px" height="40px"/> : null
+          }
+          {
+            data.main && data.weather[0].description!=='' && (data.weather[0].description).toLowerCase().includes('clouds') ? 
+            <img style={{margin:'3px'}} src={brokenClouds} alt="..." width="40px" height="40px"/> : null
+          }
+          {
+            data.main && data.weather[0].description!=='' && (data.weather[0].description).toLowerCase().includes('thunder') ? 
+            <img style={{margin:'3px'}} src={thunder} alt="..." width="40px" height="40px"/> : null
+          }
+          {
+            data.main && data.weather[0].description!=='' && (data.weather[0].description).toLowerCase().includes('rain') ? 
+            <img style={{margin:'3px'}} src={rain} alt="..." width="40px" height="40px"/> : null
+          }
+          
           <div className="card-body">
             {
               data.main ? <h2 className="card-title">{(data.main.temp-273.15).toFixed(1)} °C</h2> : null
